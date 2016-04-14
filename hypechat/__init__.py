1ee398e9a69e26539bc7afbdabf8405e9509358d
Gert <pagt0007@hz.nl>
2016-04-14 12:14:49 +0200

    ordered the file around, added some comments, user can now start with -h or -r to request a help tool or reset the configs

--- hypechat/__init__.py
+++ hypechat/__init__.py
@@ -1,11 +1,64 @@
-import urllib2, json, urllib, sys
+import urllib2, json, urllib, sys, getopt
 from configwriter import ConfigWriter
 
-data = sys.argv
-if len(data) > 1:
-    if 'reset' in data:
-        ConfigWriter().reset()
+
+#Read initial arguments
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"hr",["help","reset"])
+except getopt.GetoptError:
+    usage()
+    sys.exit(2)
 config = ConfigWriter()
+
+
+#Sends a message to the chat
+def send_message(message,color):   
+    request = urllib2.Request(room_url+'/notification')
+    request.add_header('Authorization', 'Bearer ' + token)
+    data = {'message' : message, 'color': color,'message_format': 'text','notify': True}
+    request.add_data(urllib.urlencode(data))
+    try:
+        response = urllib2.urlopen(request)
+    except urllib2.HTTPError as e:
+        print e.read()
+
+
+def get_rooms():
+    request = urllib2.Request(url=full_url+'/v2/room')
+    request.add_header('Authorization', 'Bearer ' + token)
+    response = urllib2.urlopen(request)
+    return json.loads( response.read() )['items']
+
+
+def get_room(name):
+    for room in get_rooms():
+        if room['name'] == name:
+            return room
+
+
+def usage():
+    print "Commands:"
+    print "-h, -help: Display this message"
+    print "-r --reset: Reset user settings"
+    sys.exit()
+
+
+#checks the inital arguments                                                                    
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"hr",["help","reset"])                           
+except getopt.GetoptError:                                                                   
+    usage()
+    sys.exit(2)                                                                              
+
+#ConfigWriter object
+config = ConfigWriter()
+
+for opt, arg in opts:
+    if opt in ('-h','--help'):
+        usage()
+        sys.exit()
+    elif opt in ('-r','reset'):
+        config.reset()
 
 if config.has("Oauth2token"):
     token = config.get("Oauth2token")
@@ -19,9 +72,6 @@ else:
     roomkey = raw_input("Roomkey:")
     config.set("roomkey",roomKey)
 
-
-
-
 protocol = 'https' 
 api_url = 'api.hipchat.com'
 
@@ -29,28 +79,6 @@ print config
 room_url = 'https://api.hipchat.com/v2/room/'+roomkey
 
 full_url = protocol + '://' + api_url
-
-def send_message(message,color):
-    
-    request = urllib2.Request(room_url+'/notification')
-    request.add_header('Authorization', 'Bearer ' + token)
-    data = {'message' : message, 'color': color,'message_format': 'text','notify': True}
-    request.add_data(urllib.urlencode(data))
-    try:
-        response = urllib2.urlopen(request)
-    except urllib2.HTTPError as e:
-        print e.read()
-
-def get_rooms():
-    request = urllib2.Request(url=full_url+'/v2/room')
-    request.add_header('Authorization', 'Bearer ' + token)
-    response = urllib2.urlopen(request)
-    return json.loads( response.read() )['items']
-
-def get_room(name):
-    for room in get_rooms():
-        if room['name'] == name:
-            return room
 
 #print get_room('Random')
 mess = ''
